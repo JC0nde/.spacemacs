76b17866b75eb84daed1ae758a4db6f81c27e447
Jonathan Conde <mail@jonathanconde.com>
2016-03-08 10:45:16 +0100

    update imenu

--- config.el
+++ config.el
@@ -152,6 +152,6 @@
         "Prevent annoying \"Active processes exist\" query when you quit Emacs."
         (flet ((process-list ())) ad-do-it))
         
-(global-set-key (kbd "M-i") 'ido-goto-symbol)
+(global-set-key (kbd "M-i") 'imenu)
         
 ;;; config.el ends here

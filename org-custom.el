2798f91ee6baacc899a3d24ef4707437da832479
Jonathan Conde <mail@jonathanconde.com>
2016-10-27 01:00:41 +0200

    re-dedans

--- org-custom.el
+++ org-custom.el
@@ -1,8 +1,47 @@
 ;;; org-custom.el --- Org-mode setup to organize my evil plans
-;;; Commentary: Taken from the excellent Org Mode - Organize Your Life In Plain Text! -> http://doc.norang.ca/org-mode.html
+;;
+;; Copyright © 2016 Jonathan Conde
+;;
+;; Author: Jonathan Conde <mail@jonathanconde.com>
+;; URL: https://github.com/bbatsov/emacs-prelude
+;; Version: 0.0.1
+
+
+;; This file is not part of GNU Emacs.
+
+
+;;; Commentary:
+
+
+;; Some basic configuration for org-mode.
+;; Taken from the excellent Org Mode - Organize Your Life In Plain Text! -> http://doc.norang.ca/org-mode.html
+
+
+;;; License:
+
+;; This program is free software; you can redistribute it and/or
+;; modify it under the terms of the GNU General Public License
+;; as published by the Free Software Foundation; either version 3
+;; of the License, or (at your option) any later version.
+;;
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+;;
+;; You should have received a copy of the GNU General Public License
+;; along with GNU Emacs; see the file COPYING.  If not, write to the
+;; Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+;; Boston, MA 02110-1301, USA.
+
+
 ;;; Code:
+
+
+;; Org basic setup
 (add-to-list 'auto-mode-alist '("\\.\\(org\\|org_archive\\|txt\\)$" . org-mode))
 (require 'org)
+
 
 ;; Variables for the org-user-agenda-files
 (if (boundp 'org-user-agenda-files)

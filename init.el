eb939fd94578595a630e7d51dbc188e719a54b78
Jonathan Conde <mail@jonathanconde.com>
2016-11-13 18:20:45 +0100

    update github file

--- init.el
+++ init.el
@@ -1,4 +1,4 @@
-;;; config.el --- Config Emacs.
+;;; init.el --- Config Emacs.
 
 ;; This file is not part of GNU Emacs.
 
@@ -12,22 +12,29 @@
 
 ;;; Code:
 
-;;Add MELPA repository for packages
-(add-to-list 'package-archives
-             '("melpa" . "https://melpa.org/packages/") t)
-;; org contrib
-(add-to-list 'package-archives
-             '("org" . "http://orgmode.org/elpa/") t)
+(setenv "LANG" "fr_CH.UTF-8")
 
-(when (< emacs-major-version 24)
-  ;; For important compatibility libraries like cl-lib
-  (add-to-list 'package-archives
-               '("gnu" . "https://elpa.gnu.org/packages/")))
+(require 'package)
+(setq package-enable-at-startup nil)
+(add-to-list 'package-archives
+             '("melpa" . "https://melpa.org/packages/"))
+
 (package-initialize)
+
+;; Bootstrap `use-package'
+(unless (package-installed-p 'use-package)
+  (package-refresh-contents)
+  (package-install 'use-package))
+
+(use-package try
+             :ensure t)
+
+(setq prelude-flyspell nil)
+;; (setq ispell-program-name "/usr/local/bin/aspell")
 
 ;; install additional packages - add any to this list that you want to
 ;; be installed automatically
-(prelude-require-packages '(multiple-cursors ess emmet-mode golden-ratio bbdb goto-chg org-bullets))
+(prelude-require-packages '(multiple-cursors ess emmet-mode golden-ratio bbdb goto-chg solarized-theme))
 
 ;;smooth scrolling
 (setq prelude-use-smooth-scrolling t)
@@ -35,15 +42,13 @@
 ;; No scroll bar
 (scroll-bar-mode -1)
 
-;;linum t
 (global-linum-mode t)
 
-;; Disable flyspell-mode
-(setq prelude-flyspell nil)
-
-;;No whitespaces
+;;No whitespace
 (setq prelude-whitespace nil)
 
+;;don't highlight the end of long lines
+(setq whitespace-line-column 99999)
 
 ;;Backups
 (setq vc-make-backup-files t)
@@ -55,6 +60,7 @@
 
 (setq backup-directory-alist '(("" . "~/.emacs.d/personal/backups/per-save")))
 (defun force-backup-of-buffer ()
+  "."
   (when (not buffer-backed-up)
     (let ((backup-directory-alist '(("" . "~/.emacs.d/personal/backups/per-session")))
           (kept-new-versions 3))
@@ -63,12 +69,10 @@
     (backup-buffer)))
 (add-hook 'before-save-hook  'force-backup-of-buffer)
 
-
 ;; undo
 (global-set-key (kbd "C-z") 'undo)
 
-;;Guru-mode
-;;enable arrow keys
+;;Guru-mode off
 (setq prelude-guru nil)
 
 ;;Multiple-cursors
@@ -77,9 +81,26 @@
 (global-set-key (kbd "C-<") 'mc/mark-previous-like-this)
 (global-set-key (kbd "C-c C-<") 'mc/mark-all-like-this)
 
+;; Web-mode
 (require 'web-mode)
-(add-to-list 'auto-mode-alist '("\\.php\\'" . web-mode))
+(setq web-mode-markup-indent-offset 4)
+(setq web-mode-css-indent-offset 2)
+(setq web-mode-code-indent-offset 4)
+(setq web-mode-disable-autocompletion t)
+(local-set-key (kbd "RET") 'newline-and-indent)
 
+;; Disable whitespace-mode when using web-mode
+(add-hook 'web-mode-hook (lambda () (whitespace-mode -1)))
+(add-to-list 'auto-mode-alist '("\\.inc$" . php-mode)) ;; - For Drupal
+(add-to-list 'auto-mode-alist '("\\.\\(module\\|test\\|install\\|theme\\)$" . php-mode)) ;; - For Drupal
+(add-to-list 'auto-mode-alist '("\\.phtml\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.tpl\\.php\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.blade\\.php\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.jsp\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.as[cp]x\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.erb\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.html?\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("/\\(views\\|html\\|theme\\|templates\\)/.*\\.php\\'" . web-mode))
 
 ;;Emmet
 (require 'emmet-mode)
@@ -93,14 +114,42 @@
 (global-set-key [(control ?.)] 'goto-last-change)
 (global-set-key [(control ?,)] 'goto-last-change-reverse)
 
+
 ;; C-h => Backspace
 (keyboard-translate ?\C-h ?\C-?)
 ;; M-h => backward-kill-word
-(global-set-key (kbd "M-h" ) 'backward-kill-word)
-;; M-<DEL> => mark paragraph
-(global-set-key (kbd "M-<DEL>" ) 'mark-paragraph)
+(global-set-key (kbd "M-h") 'backward-kill-word)
+;; M-<DEL> =>
+(global-set-key (kbd "M-<DEL>") 'mark-paragraph)
+
 
 (defalias 'qrr 'query-replace-regexp)
+
+(setq ring-bell-function 'ignore)
+
+(global-set-key (kbd "C-S-<mouse-1>") 'mc/add-cursor-on-click)
+
+;; make both fringes 4 pixels wide
+(fringe-mode 4)
+(setq next-line-add-newlines t)
+
+(defadvice save-buffers-kill-emacs (around no-query-kill-emacs activate)
+  "Prevent annoying \"Active processes exist\" query when you quit Emacs."
+  (flet ((process-list ())) ad-do-it))
+
+(global-set-key (kbd "M-i") 'imenu)
+
+;;Right meta-modifier-tweak for IOS
+(setq ns-right-alternate-modifier 'none)
+
+(defun goto-match-paren (arg)
+  "Go to the matching parenthesis ( ARG ) if on parenthesis, otherwise insert %.
+vi style of % jumping to matching brace."
+  (interactive "p")
+  (cond ((looking-at "\\s\(") (forward-list 1) (backward-char 1))
+        ((looking-at "\\s\)") (forward-char 1) (backward-list 1))
+        (t (self-insert-command (or arg 1)))))
+(global-set-key (kbd "C-%") 'goto-match-paren)
 
 ;; This works a little better if isearch puts you at the start of the search, not the end:
 (add-hook 'isearch-mode-end-hook 'my-goto-match-beginning)
@@ -113,7 +162,7 @@
 (global-set-key (kbd "M-x") 'helm-M-x)
 (key-chord-define-global "xx" 'helm-M-x)
 (key-chord-define-global "yy" 'helm-show-kill-ring)
-;; Skip files in dired  
+;; Skip files in dired
 (progn
   (setq dired-omit-verbose nil)
   ;; toggle `dired-omit-mode' with C-x M-o
@@ -127,4 +176,4 @@
 
 (setq mouse-drag-copy-region t)
 
-;;; config.el ends here
+;;; init.el ends here

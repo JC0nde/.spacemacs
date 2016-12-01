007395499a98ba332c3507cdc14d94263d71b080
Jonathan Conde <mail@jonathanconde.com>
2016-12-01 02:38:31 +0100

    fix identation in web and js2 modes

--- init.el
+++ init.el
@@ -113,6 +113,19 @@
             ("html" . (ac-source-words-in-buffer ac-source-abbrev))))
     ))
 
+;; adjust indents for web-mode to 2 spaces
+(defun my-web-mode-hook ()
+  "Hooks for Web mode. Adjust indents"
+  ;;; http://web-mode.org/
+  (setq web-mode-markup-indent-offset 2)
+  (setq web-mode-css-indent-offset 2)
+  (setq web-mode-code-indent-offset 2))
+(add-hook 'web-mode-hook  'my-web-mode-hook)
+
+(custom-set-variables  
+ '(js2-basic-offset 2)  
+ '(js2-bounce-indent-p t)
+ 
 (define-key global-map (kbd "RET") 'newline-and-indent)
 
 
